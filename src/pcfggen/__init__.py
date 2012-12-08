a92000a956a9f1da880ddb5b4f9837a0726c8a9d
Todd Layton <tlayton@mit.edu>
2012-12-08 00:48:02 -0500

    incorporated nltk tagger and restructured for nltk corpora

--- src/pcfggen/__init__.py
+++ src/pcfggen/__init__.py
@@ -1,11 +1,11 @@
-import nltk.grammar
+import nltk.grammar, nltk.tag
 import uuid
 
 
 
-def generate_pcfg(corpus, vocab):
-    pos_corpus = generate_pos_corpus(corpus, vocab)
-    grammar = generate_exhaustive_grammar(pos_corpus)
+def generate_pcfg(tagged_sents):
+##    pos_corpus = generate_pos_corpus(sents)
+    grammar = generate_exhaustive_grammar(tagged_sents)
     while True:
         n_gram, freq = get_best_n_gram(grammar)
         if n_gram is not None and is_above_threshold(freq):
@@ -15,24 +15,23 @@ def generate_pcfg(corpus, vocab):
     return grammar
 
 
-def generate_pos_corpus(corpus, vocab):
-    '''Uses a POS vocabulary to convert a sentence corpus
-       into a POS-tag-sequence corpus'''
-    pos_corpus = []
-    for sent in corpus:
-        #TODO: use vocab (or tagger) to produce seqs and probs and add to pos_corpus
-        pos_corpus.append((1.0 / len(corpus), sent.split()))
-    return pos_corpus
+##def generate_pos_corpus(sents):
+##    '''Uses a POS tagger to convert a sentence corpus
+##       into a POS-tag-sequence corpus'''
+##    return nltk.tag.batch_pos_tag(sents)
 
 
-def generate_exhaustive_grammar(pos_corpus, start = 'ROOT'):
+def generate_exhaustive_grammar(tagged_sents, start = 'ROOT'):
     '''Converts a probabilistic POS-tag-sequence corpus into an exhaustive CFG'''
     prods = []
-    for prob, seq in pos_corpus:
-        prod = nltk.grammar.parse_pcfg_production("{0} -> {1} [{2}]".format(start, " ".join(seq), prob))[0]
+    start = nltk.grammar.Nonterminal(start)
+    for tagged_sent in tagged_sents:
+        prod = nltk.grammar.WeightedProduction(start, [nltk.grammar.Nonterminal(tag) for token, tag in tagged_sent], prob = 1.0 / len(tagged_sents))
         prod.freq = prod.prob()
         prods.append(prod)
-    return nltk.grammar.WeightedGrammar(start, prods)
+    grammar = nltk.grammar.WeightedGrammar(start, prods)
+    grammar.new_tag_count = 0
+    return grammar
 
 
 def get_best_n_gram(grammar, n = 2):
@@ -47,7 +46,6 @@ def get_best_n_gram(grammar, n = 2):
             if n_gram not in n_grams:
                 n_grams[n_gram] = 0
             n_grams[n_gram] += prod.freq
-        pass
     if not n_grams:
         return (None, None)
     else:
@@ -56,15 +54,15 @@ def get_best_n_gram(grammar, n = 2):
 
 def is_above_threshold(freq):
     '''Checks whether a relative frequency is high enough'''
-    #TODO: return comparison to some selected probability
-    return freq >= 1.0
+    return freq >= 0.0
 
 
 def create_non_terminal(grammar, n_gram, freq):
     n_gram = list(n_gram)
     '''Replaces an n-gram in a grammar with a new non-terminal'''
     prods = []
-    nt = nltk.grammar.Nonterminal('{0}-{1}'.format('-'.join(nt.symbol() for nt in n_gram), uuid.uuid1()))
+    nt = nltk.grammar.Nonterminal('_'.join("({0})".format(nt.symbol()) for nt in n_gram))
+    grammar.new_tag_count += 1
     for prod in grammar.productions():
         rhs = list(prod.rhs())
         i = 0
@@ -78,4 +76,6 @@ def create_non_terminal(grammar, n_gram, freq):
     new_prod = nltk.grammar.WeightedProduction(nt, n_gram, prob = 1.0)
     new_prod.freq = freq
     prods.append(new_prod)
-    return nltk.grammar.WeightedGrammar(grammar.start(), prods)
+    new_grammar = nltk.grammar.WeightedGrammar(grammar.start(), prods)
+    new_grammar.new_tag_count = grammar.new_tag_count
+    return new_grammar
