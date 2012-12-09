1bdb4e57c379c8bcd494acd49262c08bbb84d304
Todd Layton <tlayton@mit.edu>
2012-12-08 22:19:53 -0500

    added probabilistic similar-rule joining

--- src/pcfggen/__init__.py
+++ src/pcfggen/__init__.py
@@ -4,21 +4,20 @@ import uuid
 
 
 def generate_pcfg(tagged_sents):
-##    pos_corpus = generate_pos_corpus(sents)
     grammar = generate_exhaustive_grammar(tagged_sents)
     while True:
+        while True:
+            diff_index, base_prod = find_join(grammar)
+            if diff_index is not None:
+                grammar = add_join_nonterminal(grammar, diff_index, base_prod)
+            else:
+                break
         n_gram, freq = get_best_n_gram(grammar)
-        if n_gram is not None and is_above_threshold(freq):
-            grammar = create_non_terminal(grammar, n_gram, freq)
+        if n_gram is not None:
+            grammar = add_sub_nonterminal(grammar, n_gram, freq)
         else:
             break
     return grammar
-
-
-##def generate_pos_corpus(sents):
-##    '''Uses a POS tagger to convert a sentence corpus
-##       into a POS-tag-sequence corpus'''
-##    return nltk.tag.batch_pos_tag(sents)
 
 
 def generate_exhaustive_grammar(tagged_sents, start = 'ROOT'):
@@ -30,7 +29,7 @@ def generate_exhaustive_grammar(tagged_sents, start = 'ROOT'):
         prod.freq = prod.prob()
         prods.append(prod)
     grammar = nltk.grammar.WeightedGrammar(start, prods)
-    grammar.new_tag_count = 0
+    grammar.new_symbol_count = 0
     return grammar
 
 
@@ -52,17 +51,12 @@ def get_best_n_gram(grammar, n = 2):
         return max(n_grams.items(), key = lambda item: item[1])
 
 
-def is_above_threshold(freq):
-    '''Checks whether a relative frequency is high enough'''
-    return freq >= 0.0
-
-
-def create_non_terminal(grammar, n_gram, freq):
-    n_gram = list(n_gram)
+def add_sub_nonterminal(grammar, n_gram, freq):
     '''Replaces an n-gram in a grammar with a new non-terminal'''
+    n_gram = list(n_gram)
     prods = []
-    nt = nltk.grammar.Nonterminal('_'.join("({0})".format(nt.symbol()) for nt in n_gram))
-    grammar.new_tag_count += 1
+    nt = nltk.grammar.Nonterminal('sub_{0}_{1}'.format('_'.join("({0})".format(nt.symbol()) for nt in n_gram), grammar.new_symbol_count))
+    grammar.new_symbol_count += 1
     for prod in grammar.productions():
         rhs = list(prod.rhs())
         i = 0
@@ -71,11 +65,57 @@ def create_non_terminal(grammar, n_gram, freq):
                 rhs[i:i + len(n_gram)] = [nt]
             i += 1
         new_prod = nltk.grammar.WeightedProduction(prod.lhs(), rhs, prob = prod.prob())
-        new_prod.freq = prod.freq * prod.prob()
+        new_prod.freq = prod.freq
         prods.append(new_prod)
     new_prod = nltk.grammar.WeightedProduction(nt, n_gram, prob = 1.0)
     new_prod.freq = freq
     prods.append(new_prod)
     new_grammar = nltk.grammar.WeightedGrammar(grammar.start(), prods)
-    new_grammar.new_tag_count = grammar.new_tag_count
+    new_grammar.new_symbol_count = grammar.new_symbol_count
+    return new_grammar
+
+def find_join(grammar):
+    for i in range(len(grammar.productions())):
+        prod1 = grammar.productions()[i]
+        if len(prod1.rhs()) < 2:
+            continue
+        for j in range(i + 1, len(grammar.productions())):
+            prod2 = grammar.productions()[j]
+            if len(prod1.rhs()) != len(prod2.rhs()):
+                continue
+            diff_index = None
+            for k in range(len(prod1.rhs())):
+                if prod1.rhs()[k] != prod2.rhs()[k]:
+                    if diff_index is None:
+                        diff_index = k
+                    else:
+                        diff_index = None
+                        break
+            if diff_index is not None:
+                return (diff_index, prod1)
+    return (None, None)
+
+def add_join_nonterminal(grammar, diff_index, base_prod):
+    prods = []
+    new_prods = []
+    for prod in grammar.productions():
+        if len(prod.rhs()) == len(base_prod.rhs()) and all(prod.rhs()[i] == base_prod.rhs()[i] or i == diff_index for i in range(len(prod.rhs()))):
+            prods.append(prod)
+        else:
+            new_prods.append(prod)
+    nt = nltk.grammar.Nonterminal('join_{0}_{1}'.format('_'.join("({0})".format(prod.rhs()[diff_index]) for prod in prods), grammar.new_symbol_count))
+    grammar.new_symbol_count += 1
+    total_freq = sum(prod.freq for prod in prods)
+    new_rhs = list(base_prod.rhs())
+    new_rhs[diff_index] = nt
+    for prod in prods:
+        new_prod = nltk.grammar.WeightedProduction(prod.lhs(), new_rhs, prob = prod.prob())
+        new_prod.freq = prod.freq
+        new_prods.append(new_prod)
+        
+        new_prod = nltk.grammar.WeightedProduction(nt, [prod.rhs()[diff_index]], prob = prod.freq / total_freq)
+        new_prod.freq = prod.freq
+        new_prods.append(new_prod)
+    new_grammar = nltk.grammar.WeightedGrammar(grammar.start(), new_prods)
+    new_grammar.new_symbol_count = grammar.new_symbol_count
     return new_grammar
