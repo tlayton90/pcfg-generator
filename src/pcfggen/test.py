b1d391175cf3a27e6c63fbdc08b213bf990276e8
staicy6 <staicy6@yandex.ru>
2012-12-10 11:56:40 -0500

    Fixed everything besides taking our grammar
    Will continue today afternoon

--- src/pcfggen/test.py
+++ src/pcfggen/test.py
@@ -10,7 +10,7 @@ from nltk.treetransforms import *
 #creating PCFG from parsed sentences
 #used for golden standart pcfg
 def learn_treebank(files=None, markov_order=None):
-    #default is reading from treebank.parsed_sents()
+    #default is reading from treebank.parsed_sents() - 10% of penn Tree bank
     if files is None: bank = treebank.parsed_sents()
     else: bank = treebank.parsed_sents(files)
     return learn_trees_gs(bank, collapse=True, markov_order=markov_order)
@@ -19,43 +19,50 @@ def learn_treebank(files=None, markov_order=None):
 def learn_trees_gs(trees, collapse=True, markov_order=None):
     productions = []
     for tree in trees:
-      if collapse: tree.collapse_unary(collapsePOS=False)
-      if markov_order:
-        tree.chomsky_normal_form(horzMarkov=markov_order)
-      else:
-        tree.chomsky_normal_form()
-      productions += tree.productions()
+        if collapse: tree.collapse_unary(collapsePOS=False)
+        if markov_order:
+            tree.chomsky_normal_form(horzMarkov=markov_order)
+        else:
+            tree.chomsky_normal_form()
+        productions += tree.productions()
     grammar = nltk.grammar.induce_pcfg(Nonterminal('S'), productions)
     return grammar
 
 #make our PCFG
 #takes a file with parsed trees taking them as an argument from command line
-def learn_pcfg_res(file_input):
-    grammar = __init__.generate_pcfg(file_input)
+def learn_pcfg_res(file_input=None):
+	if file_input is None: bank = treebank.parsed_sents()
+	else: bank = treebank.parsed_sents(file_input)
+    grammar = __init__.generate_pcfg(bank)
     return grammar
 
 
 #function for viterbi parsing. returns probability for sentence
-def parse_viterbi_with_pcfg (grammar_input, sentence):
+def parse_viterbi_with_pcfg (grammar_input, sentence, flag_pred):
     #converts grammar into neccessary format
-    grammar = nltk.parse_pcfg(grammar_input)    
+	if flag_pred==1:
+        grammar = nltk.parse_pcfg(grammar_input)
+	else: grammar = grammar_input
     viterbi_parser = nltk.ViterbiParser(grammar)
     viterbiTree=viterbi_parser.parse(sentence.split())
-    probability = re.findall(r"[-+]?\d*\.\d+|\d+", str(i))
+    probability = re.findall(r"[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?", str(viterbiTree))
     return probability[-1]
 
 
-
 sentences = file(sys.argv[1])
+averageProb = 0
+amountOfSents = 0
 for i in sentences:
     grammar_gs = learn_treebank(sys.argv[2], None)
     grammar_pred = learn_pcfg_res(sys.argv[2])
     probab_gs = parse_viterbi_with_pcfg(grammar_gs, i)
     probab_pred = parse_viterbi_with_pcfg(grammar_pred, i)
-    print i, abs(probab_gs-probab_pred)
+    difference = abs(float(probab_gs) - float(probab_pred))
+    print i, difference
+    amountOfSents+=1
+    averageProb+=difference
 
+print "Result Average Difference: ", averageProb/amountOfSents
 
 
 #sent = treebank.tagged_sents()
-
-
