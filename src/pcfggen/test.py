7e8aa2797b685692f36cc854013c30d7fc2c405b
staicy6 <staicy6@yandex.ru>
2012-12-11 17:35:09 -0500

    fixed almost everything

--- src/pcfggen/test.py
+++ src/pcfggen/test.py
@@ -31,23 +31,19 @@ def learn_trees_gs(trees, collapse=True, markov_order=None):
 #make our PCFG
 #takes a file with parsed trees taking them as an argument from command line
 def learn_pcfg_res(file_input=None):
-	if file_input is None: bank = treebank.parsed_sents()
-	else: bank = treebank.parsed_sents(file_input)
+    if file_input is None: bank = treebank.tagged_sents()[:1]
+    else: bank = treebank.tagged_sents(file_input)
     grammar = __init__.generate_pcfg(bank)
     return grammar
 
 
 #function for viterbi parsing. returns probability for sentence
-def parse_viterbi_with_pcfg (grammar_input, sentence, flag_pred):
+def parse_viterbi_with_pcfg (grammar, sentence, flag_pred):
     #converts grammar into neccessary format
-	if flag_pred==1:
-        grammar = nltk.parse_pcfg(grammar_input)
-	else: grammar = grammar_input
     viterbi_parser = nltk.ViterbiParser(grammar)
     viterbiTree=viterbi_parser.parse(sentence.split())
     probability = re.findall(r"[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?", str(viterbiTree))
     return probability[-1]
-
 
 sentences = file(sys.argv[1])
 averageProb = 0
@@ -56,7 +52,7 @@ for i in sentences:
     grammar_gs = learn_treebank(sys.argv[2], None)
     grammar_pred = learn_pcfg_res(sys.argv[2])
     probab_gs = parse_viterbi_with_pcfg(grammar_gs, i)
-    probab_pred = parse_viterbi_with_pcfg(grammar_pred, i)
+    probab_pred = parse_viterbi_with_pcfg(grammar_pred, i, 1)
     difference = abs(float(probab_gs) - float(probab_pred))
     print i, difference
     amountOfSents+=1
@@ -66,3 +62,5 @@ print "Result Average Difference: ", averageProb/amountOfSents
 
 
 #sent = treebank.tagged_sents()
+
+
